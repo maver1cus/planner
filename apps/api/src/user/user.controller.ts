953d3a36858bfb8d1e0a04fb3844a4ae86de8d0a
Sergey Petrov <mavericusdev@gmail.com>
2021-12-03 13:44:41 +0300

    create endpoint login

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
+import {LoginUserDto} from "@app/user/dto/login-user.dto";
+import {UserResponseInterface} from "@app/user/types/user-response.interface";
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('login')
+  async login(
+    @Body('user') loginUserDto: LoginUserDto
+  ): Promise<any> {
+    const user = await this.userService.login(loginUserDto);
+
+    return this.userService.buildUserResponse(user);
+  }
 }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,12 +1,68 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from '@app/user/user.entity';
-import { Repository } from 'typeorm';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {UserEntity} from '@app/user/user.entity';
+import {Repository} from 'typeorm';
+import {LoginUserDto} from "@app/user/dto/login-user.dto";
+import {UserResponseInterface} from "@app/user/types/user-response.interface";
+import {sign} from "jsonwebtoken";
+import {JWT_SECRET} from "@app/config";
+import {compare} from "bcrypt";
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userEntity: Repository<UserEntity>,
-  ) {}
+    private readonly userRepository: Repository<UserEntity>,
+  ) {
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      login: loginUserDto.login
+    }, {
+      select: ['id', 'login', 'password']
+    });
+
+    if (!user) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY
+      )
+    }
+
+    const isPasswordCorrect = await compare(
+      loginUserDto.password,
+      user.password
+    )
+
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY
+      )
+    }
+
+    delete user.password;
+
+    return user;
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        login: user.login
+      },
+      JWT_SECRET
+    )
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user)
+      },
+    }
+  }
 }
