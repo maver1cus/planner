068a3b59ddce69ab165b90782184e219cd5e854a
Sergey Petrov <mavericusdev@gmail.com>
2021-12-25 19:39:29 +0300

    refactor AuthMiddelware

--- apps/api/src/user/middelware/auth.middelware.ts
+++ apps/api/src/user/middelware/auth.middelware.ts
@@ -10,23 +10,19 @@ export class AuthMiddelware implements NestMiddleware {
   constructor(private readonly userService: UserService) {}
 
   async use(req: ExpressRequestInterface, res: Response, next: NextFunction) {
-    if (!req.headers.authorization) {
-      req.user = null;
-      next();
+    const { authorization } = req.headers;
+    req.user = null;
 
-      return;
+    if (authorization) {
+      try {
+        const token = authorization.split(' ')[1];
+        const { id } = verify(token, JWT_SECRET) as { id: number };
+        req.user = await this.userService.findById(id);
+      } catch (e) {
+        req.user = null;
+      }
     }
 
-    const token = req.headers.authorization.split(' ')[1];
-
-    try {
-      const decode = verify(token, JWT_SECRET) as { id: number };
-      const user = await this.userService.findById(decode.id);
-      req.user = user;
-      next();
-    } catch (e) {
-      req.user = null;
-      next();
-    }
+    next();
   }
 }
