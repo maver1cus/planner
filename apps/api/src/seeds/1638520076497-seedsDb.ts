3759b122b7b0b624904e600130d4878c02e02267
Sergey Petrov <mavericusdev@gmail.com>
2022-01-12 16:39:29 +0300

    add Task seed

--- apps/api/src/seeds/1638520076497-seedsDb.ts
+++ apps/api/src/seeds/1638520076497-seedsDb.ts
@@ -10,6 +10,10 @@ export class SeedDb1638520076497 implements MigrationInterface {
     await queryRunner.query(
       `INSERT INTO users (login, password) VALUES ('foo', '${password}')`,
     );
+
+    await queryRunner.query(
+      `INSERT INTO tasks (id, title, "dateStart", done, type, "parentId", author) VALUES (DEFAULT, 'root', null, false, 'folder'::tasks_type_enum, 0, 1)`,
+    );
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
