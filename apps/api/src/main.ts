e2184e46a7b587d66a982ba5907b70cac222d336
Sergey Petrov <mavericusdev@gmail.com>
2022-01-18 18:21:36 +0300

    add CORS to server

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,6 +9,10 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Planner')
