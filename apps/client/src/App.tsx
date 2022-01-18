a2d9ce45a2a0e8acca1141052d148f1569335d2c
Sergey Petrov <mavericusdev@gmail.com>
2022-01-18 15:13:57 +0300

    add ant design

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Layout } from 'antd'
 import AppRouter from "./components/app-router/app-router";
 
 function App() {
   return (
-    <>
+    <Layout>
       <AppRouter />
-    </>
+    </Layout>
   );
 }
 
